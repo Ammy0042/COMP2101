cd62827ea30ed467a1a8f9daa8519127080d75c6
Ammy0042 <comp2101-02@zonzorp.net>
2022-09-28 13:27:03 -0400

    Working with bash

---
+++ bash/sysinfo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Administrative Scripting COMP2101
+#Bash 
+#lab 1
+echo "Hostname"
+echo "pc200497165"
+#Command for checking the system informartion
+hostnamectl
+#Command for checking the IPv4 address  
+echo "IPv4 address"
+#Command for checking the IPv6 address
+echo "IPv6 address"
+ip a s ens33 | grep -w inet | awk '{print $2}'
+ip a s ens33 | grep -w inet6 | awk '{print $2}'
+#Command to check the total space & available space on the system
+df
+# The end
